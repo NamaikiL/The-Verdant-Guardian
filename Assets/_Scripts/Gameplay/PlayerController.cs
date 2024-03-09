c1a39229a760b5d0626b9d57fefa86656bdd792f
Meinu <74197474+NamaikiL@users.noreply.github.com>
2024-03-09 22:24:49 +0100

    Implémentation Dev

--- Assets/_Scripts/Gameplay/PlayerController.cs
+++ Assets/_Scripts/Gameplay/PlayerController.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using _Scripts.Managers;
 using _Scripts.Scriptables;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 namespace _Scripts.Gameplay
